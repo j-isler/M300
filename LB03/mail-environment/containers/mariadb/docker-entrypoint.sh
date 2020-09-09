b87d4c16d1b54614314638d70dbc5a10319bb3b4
Jenia Isler <jenia.isler@gmx.ch>
2020-09-09 21:45:34 +0200

    Update docker-entrypoint.sh

--- LB03/mail-environment/containers/mariadb/docker-entrypoint.sh
+++ LB03/mail-environment/containers/mariadb/docker-entrypoint.sh
@@ -5,6 +5,7 @@ password=$POSTFIXADMIN_PASSWORD
 echo "configuring mariadb database..." 
 sed -i "s/password/$password/g" /postfixadmin-script.sql
 
+mysql -u root -psecret < /postfixadmin-script.sql
 
 # https://stackoverflow.com/questions/46482257/how-to-add-a-startup-script-to-a-mysql-docker-container
 
