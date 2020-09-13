9b347b820cdcbfe581e5f57e561bbf458c7245a7
Baru <baruyr.bousnaian@edu.tbz.ch>
2020-09-13 15:48:34 +0200

    updated entrypoint for postfix

--- LB03/mail-environment/containers/postfix/docker-entrypoint.sh
+++ LB03/mail-environment/containers/postfix/docker-entrypoint.sh
@@ -58,26 +58,26 @@ echo "smtp inet n - y - - smtpd
 #dnsblog unix - - y - 0 dnsblog
 #tlsproxy unix - - y - 0 tlsproxy
 submission inet n - y - - smtpd
--o syslog_name=postfix/submission
--o smtpd_tls_security_level=encrypt
--o smtpd_sasl_auth_enable=yes
+	-o syslog_name=postfix/submission
+	-o smtpd_tls_security_level=encrypt
+	-o smtpd_sasl_auth_enable=yes
 # -o smtpd_reject_unlisted_recipient=no
 # -o smtpd_client_restrictions=$mua_client_restrictions
 # -o smtpd_helo_restrictions=$mua_helo_restrictions
 # -o smtpd_sender_restrictions=$mua_sender_restrictions
 # -o smtpd_recipient_restrictions=
 # -o smtpd_relay_restrictions=permit_sasl_authenticated,reject
--o smtpd_client_restrictions=permit_sasl_authenticated,reject
--o milter_macro_daemon_name=ORIGINATING
+	-o smtpd_client_restrictions=permit_sasl_authenticated,reject
+	-o milter_macro_daemon_name=ORIGINATING
 smtps inet n - y - - smtpd
--o syslog_name=postfix/smtps
+	-o syslog_name=postfix/smtps
 # -o smtpd_tls_wrappermode=yes
--o smtpd_sasl_auth_enable=yes
+	-o smtpd_sasl_auth_enable=yes
 # -o smtpd_reject_unlisted_recipient=no
--o smtpd_client_restrictions=permit_sasl_authenticated,reject
+	-o smtpd_client_restrictions=permit_sasl_authenticated,reject
 # -o smtpd_client_restrictions=$mua_client_restrictions
 # -o smtpd_helo_restrictions=$mua_helo_restrictions
 # -o smtpd_sender_restrictions=$mua_sender_restrictions
 # -o smtpd_recipient_restrictions=
 # -o smtpd_relay_restrictions=permit_sasl_authenticated,reject
--o milter_macro_daemon_name=ORIGINATING" > /etc/postfix/master.cf
+	-o milter_macro_daemon_name=ORIGINATING" > /etc/postfix/master.cf
