840752824567e5bc34a2e461f63a64b925702374
drxlxn <drkras07@gmail.com>
2020-09-16 15:01:30 +0200

    Postfixadmin V2.5

---
+++ LB03/mail-environment/containers/postfix-admin/config.local.php
@@ -0,0 +1,12 @@
+<?php
+                $CONF['database_type'] = 'mysqli';
+                $CONF['database_host'] = '172.17.0.3';
+                $CONF['database_port'] = '3306';
+                $CONF['database_user'] = 'root';
+                $CONF['database_password'] = 'toor';
+                $CONF['database_name'] = 'postfixadmin';
+                $CONF['setup_password'] = 'f0a56f9f9664aaa493733144055acf1f:4c811aa4b38d9d1b0098438b8c98e43c4b164428';
+                $CONF['smtp_server'] = 'localhost';
+                $CONF['smtp_port'] = '25';
+                $CONF['configured'] = true;
+                ?>
--- LB03/mail-environment/containers/postfix-admin/docker-entrypoint.sh
+++ LB03/mail-environment/containers/postfix-admin/docker-entrypoint.sh
@@ -1,13 +1,10 @@
-set -e
+#set -e
 
 
-echo "<?php
-$CONF['database_type'] = 'mysqli';
-$CONF['database_host'] = '172.17.0.3';
-$CONF['database_user'] = 'root';
-$CONF['database_password'] = 'toor';
-$CONF['database_name'] = 'postfixadmin';
-$CONF['configured'] = true;
-$CONF['setup_password'] = 'f0a56f9f9664aaa493733144055acf1f:4c811aa4b38d9d1b0098438b8c98e43c4b164428';
-?>" > /var/www/html/config.inc.php
-# chown -R www-data: /var/www/html/postfixadmin
+#echo "<?php
+#$CONF['setup_password'] = 'f0a56f9f9664aaa493733144055acf1f:4c811aa4b38d9d1b0098438b8c98e43c4b164428';
+#?>" > /var/www/html/config.inc.php
+#chown -R www-data: /var/www/html/postfixadmin
+
+sed -i "s/'791eb4ead7fd996c01bed30707ae27dd:b7910d09773104bf84c4f4951205d2198c7cfc4f'/'f0a56f9f9664aaa493733144055acf1f:4c811aa4b38d9d1b0098438b8c98e43c4b164428'/" /var/www/html/config.local.php
+
